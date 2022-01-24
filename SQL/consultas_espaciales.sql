38040977094be0d2bc9288b68c3111ab596c677f
MetziLuna <48043166+MetziLuna@users.noreply.github.com>
2022-01-24 14:06:05 -0600

    Create consultas_espaciales.sql

---
+++ SQL/consultas_espaciales.sql
@@ -0,0 +1,22 @@
+SELECT m.cod_mun4, m.nom_mun, e.nombre_cen, e.geom
+from escuelas_lcc as e, municipios_lcc as m
+WHERE m.pobr_fisdl = 'Pobreza Severa'
+      AND ST_WITHIN(e.geom, m.geom)
+ORDER BY m.nom_mun ASC
+------------------------------------------
+SELECT distinct(l.nom), l.geom
+FROM lagos_lcc as l, municipios_lcc as m
+WHERE ST_OVERLAPS(l.geom, m.geom)
+ORDER BY l.nom
+------------------------------------------
+SELECT e.nombre_cen, e.geom
+FROM escuelas_lcc as e, rios_lcc as r
+WHERE e.municipio = 'LA LIBERTAD' AND r.nom = 'RIO GRANDE'
+      AND ST_CONTAINS(ST_BUFFER(r.geom, 700), e.geom)
+ORDER BY e.nombre_cen    
+------------------------------------------
+SELECT e.nombre_cen, e.geom
+FROM escuelas_lcc as e, rios_lcc as r
+WHERE e.municipio = 'LA LIBERTAD' AND r.nom = 'RIO GRANDE'
+      AND ST_DWITHIN(e.geom,r.geom, 700)
+ORDER BY e.nombre_cen
